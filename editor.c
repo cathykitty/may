12bcc53733d9541aec5e189cc44e8088d266626c
Kimsunyoung <cathykitty@naver.com>
2014-06-02 13:10:58 +0900

    monday

--- editor.c
+++ editor.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<termios.h>
 #include<unistd.h>
+#include<stdlib.h>
 
 /* simple line editor */
 /* when it begins, starts with command mode
@@ -9,6 +10,53 @@ initial cursor position is 1.1
 
 cursor moves left,down,up,right by 'h','j','k','l' key input
 */
+
+typedef struct ListNodeType ListNode;
+struct ListNodeType
+{
+	int data;
+	ListNode* link;
+};
+
+typedef struct LinkedListType LinkedList;
+struct LinkedListType
+{
+	int currentElement;
+	ListNode headerNode;
+};
+
+LinkedList* createLinkedList()
+{
+	LinkedList* pList;
+	pList = (LinkedList*)malloc(sizeof(LinkedList));
+	if(pList != NULL)
+	{
+		pList->currentElement=0;
+		pList->headerNode.link = NULL;
+	}
+	return pList;
+}
+void addNode(LinkedList* pList, ListNode* newNode)
+{
+	ListNode* preNode;
+	ListNode* pNewNode;
+
+ 	preNode = &(pList->headerNode);
+	pNewNode = newNode;
+	if(pList->headerNode.link == NULL)
+	{
+		pNewNode->link = preNode->link;
+		preNode->link = pNewNode;
+	}
+	else
+	{
+		while(preNode->link ! = NULL)
+		{
+			preNode = preNode-> link;
+		}
+		preNode-> link = pNewNode;
+	}
+}
 
 enum MODE {
 	COMMAND_MODE,
@@ -20,6 +68,18 @@ enum MODE {
 
 int main(int argc,char *argv[])
 {
+	int n = 0;
+	FILE* stream;
+	LinkedList* pList;
+	ListNode *Node;
+	stream = fopen("editor.txt","rt");//r인가?
+	pList = createLinkedList();
+	while(!feof(stream))
+	{
+		Node=(ListNode*)malloc(sizeof(ListNode));
+		fscanf(stream,"%c",data[][]);//여기 다시보기
+		
+
 	char buff[255];
 	enum MODE mode = COMMAND_MODE;
 
@@ -32,10 +92,34 @@ int main(int argc,char *argv[])
 	int key,ret;
 	char data[80][20];
 
+	FILE * fp =fopen("editor.txt","wt");
+	if(fp==NULL){
+		puts("file open failure!");
+		return -1;
+	}
+    //여기 수정해야함. data[][]말고 입력된 문자들로.
+	fputs("data[][]",fp);
+	fclose(fp);
+
+	//Bring stored data
+	int ch,i;
+	//파일로부터 데이터 어떻게 읽어오지?
+	FILE *fw = fopen("editor.txt","rt");
+	if(fw==NULL){
+		puts("file open failure!");
+		return -1;
+	}
+	for(i=0;i<100 ;i++)
+	{
+		ch=fgetc(fw);
+		printf("%c \n",ch);
+	}
+	fclose(fw);				
  	/*some initialization */
 	fputs("\033[2J",stdout);
 	fputs("\033[1;1H",stdout);
 
+		
 	struct termios oldt,curt,newt;
 
 	tcgetattr(STDIN_FILENO, &oldt);
@@ -119,14 +203,14 @@ int main(int argc,char *argv[])
 			case '1':
 				mode = COMMAND_MODE;
 				tcgetattr(STDIN_FILENO,&curt);
-				new = curt;
+				newt = curt;
 				newt.c_lflag &= ~(ECHO);
 				tcsetattr(STDIN_FILENO,TCSANOW,&newt);
 				break;
 			default:
 				mode = DELETE_MODE;
 				cur_col--;
-				NULL=data[cur_col +1][cur_line +1];
+			//	NULL = data[cur_col +1][cur_line +1];
 				if(cur_col<min_col){
 					cur_col=80;
 					cur_line--;
